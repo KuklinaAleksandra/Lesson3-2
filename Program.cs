cfa78a55b9f8748a7d8f32f3e9e634e74558cb9a
Aleksandra <kuklina.aleksandra@rambler.ru>
2021-04-06 22:48:46 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] matrix = {{"Ivanov", "233221"},
+                { "Petrov", "233221"},
+            { "Sidorov", "233221"},
+            { "Ivanova", "233221"},
+            { "Petrova", "233221"} };
+
+            
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 2; j++)
+
+
+                    Console.Write(matrix[i, j] + "  ");
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+
+    }
+    }
+
